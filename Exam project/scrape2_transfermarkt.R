d8723c6debe21122108e8cff83386cd441c26816
rbjoern <rbjoern@outlook.com>
2015-11-30 18:07:20 +0100

    Partly cleaned 2nd scrape
    Got zeh club names!

--- Exam project/scrape2_transfermarkt.R
+++ Exam project/scrape2_transfermarkt.R
@@ -33,15 +33,32 @@ scrape_season <- function(link) {
                     html_text()
     marketvalue <-  read_html(link)  %>%
                     html_nodes(css=css.selectors[[4]]) %>%
-                    html_text() %>% data.frame()
+                    html_text()
     otherclub   <-  read_html(link)  %>%
                     html_nodes(css=css.selectors[[5]]) %>%
-                    html_text() %>% data.frame()  
+                    html_text()
     transferfee  <- read_html(link)  %>%
                     html_nodes(css=css.selectors[[6]]) %>%
-                    html_text() %>% data.frame()  
+                    html_text()
     
   return(cbind(name, age, position, marketvalue, otherclub, transferfee))
 }
 
-df <- data.frame(scrape_season(url))
+df <-   data.frame(scrape_season(url), stringsAsFactors = FALSE)
+
+df <- df %>% mutate(club = ifelse(name==age, name, ""))
+
+#Classifies by club by identifying the lines with only club names, and changing the variable
+#TODO: Needs to count rows automatically
+for (i in 1:431) {
+  if (df$name[i] == df$age[i]) {
+    df$club[i] <- df$name[i]
+  } else {
+    df$club[i] <- df$club[i -1]
+  }
+}
+
+#After classifying, we don't need the rows only consisting of club names
+df <- df %>% filter(df$name != df$age)
+
+?ifelse
