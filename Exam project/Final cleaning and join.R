f6945521a8fde5aa81f83720e633498891b70c58
simonbjerg <simonbjerg@icloud.com>
2015-12-06 23:34:53 +0100

    Transfer data completely clean

--- Exam project/Final cleaning and join.R
+++ Exam project/Final cleaning and join.R
@@ -2,10 +2,10 @@ library("stringr")
 
 
 
-                        ################################ Data preperation ################################ 
+                        ################################ Data cleaning ################################ 
 
 # Reads player data
-players = read.csv("~/Documents/Polit/Valgfag/SDS/Gruppe2/Exam project/Data/players.csv", )
+players = read.csv("~/Documents/Polit/Valgfag/SDS/Gruppe2/Exam project/Data/players.csv" )
 # Reads transfer pricing data
 transfers = read.csv("~/Documents/Polit/Valgfag/SDS/Gruppe2/Exam project/Data/transfers.csv")
 
@@ -15,10 +15,13 @@ transfers = read.csv("~/Documents/Polit/Valgfag/SDS/Gruppe2/Exam project/Data/tr
   # Creates a year variable based on season (as character)
   players$year = substr(as.character(players$season), nchar(as.character(players$season))-4+1, nchar(as.character(players$season)))
 
+  
+  
+  
 #Preparing transfer data
   
   # Transforms some categories of transferfee to £0k
-  transfers$test = ifelse(transfers$transferfee=='Loan',
+  transfers$fee = ifelse(transfers$transferfee=='Loan',
                                                       '£0k', 
             ifelse(transfers$transferfee=='End of loan', 
                                                       '£0k',
@@ -26,39 +29,56 @@ transfers = read.csv("~/Documents/Polit/Valgfag/SDS/Gruppe2/Exam project/Data/tr
                                                       '£0k',
             as.character(transfers$transferfee)
                                                         ))) 
-  
-  
-  # Cleans the transfer pricing variable
-  transfers$test = gsub('£', "", transfers$test) 
-  transfers$test = gsub('Loan fee:', "", transfers$test) 
  
   
-  # Isolates multiplier
-  transfers$multiplier = substr(as.character(transfers$test), nchar(as.character(transfers$test))-1+1, nchar(as.character(transfers$test)))
+  # Isolates multiplier for transferfee
+  transfers$multiplierfee = substr(as.character(transfers$fee), nchar(as.character(transfers$fee))-1+1, nchar(as.character(transfers$fee)))
   
-  # Cleans multiplier
-  transfers$factor = ifelse(transfers$multiplier=="m",
+  # Cleans multiplier for transferfee
+  transfers$factorfee = ifelse(transfers$multiplierfee=="m",
                                                   as.numeric(1000000),
-                      ifelse(transfers$multiplier=="k",
+                      ifelse(transfers$multiplierfee=="k",
                                                    as.numeric(1000),
                                                    as.numeric(0)
                                                     ))
                             
-  # Extracts only digits from transfer pricing
-  transfers$test = as.numeric(str_extract(transfers$test, "\\d+\\.*\\d*"))
+  # Extracts only digits from transfer fee
+  transfers$fee = as.numeric(str_extract(transfers$fee, "\\d+\\.*\\d*"))
   
   # Creates price variable as product of transferpricing and the factor
-  transfers$price = transfers$test*transfers$factor
+  transfers$price = transfers$fee*transfers$factorfee
   
   # Cleans market value
-  transfers$test = ifelse(transfers$marketvalue=="-", NA, as.character(transfers$marketvalue))
+  transfers$MV = ifelse(transfers$marketvalue=="-", NA, as.character(transfers$marketvalue))
+  
+  # Isolates multiplier for market value
+  transfers$multiplierMV = substr(as.character(transfers$MV), nchar(as.character(transfers$MV))-1+1, nchar(as.character(transfers$MV)))
+  
+  # Cleans multiplier
+  transfers$factorMV = ifelse(transfers$multiplierMV=="m",
+                            as.numeric(1000000),
+                            ifelse(transfers$multiplierMV=="k",
+                                   as.numeric(1000),
+                                   as.numeric(0)
+                            ))
+  
+  # Extracts only digits from transfer pricing
+  transfers$MV = as.numeric(str_extract(transfers$MV, "\\d+\\.*\\d*"))
+  
+  # Creates price variable as product of transferpricing and the factor
+  transfers$marketvalue = transfers$MV*transfers$factorMV
   
   # Removes irrelevant variables
   transfers$transferfee = NULL
   transfers$test = NULL
   transfers$multiplier = NULL
   transfers$factor = NULL
-  
+  transfers$fee = NULL
+  transfers$multiplierfee = NULL
+  transfers$factorfee = NULL
+  transfers$MV =NULL
+  transfers$multiplierMV = NULL
+  transfers$factorMV = NULL
   
   
   
